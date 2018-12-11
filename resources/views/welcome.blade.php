ac8474a81c6a0f11ff5609416ec50ee14bb6bf0e
haffiz <haffiz@gates.edu.my>
2018-12-11 09:45:06 +0800

    'test'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,7 +91,7 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-                    <a href="https://github.com/laravel/laravel">Email</a>
+                    <a href="https://github.com/laravel/laravel">Email2</a>
                 </div>
             </div>
         </div>
